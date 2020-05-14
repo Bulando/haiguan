3fa346e0a49dfa2ebb2434641a914d6677a4f457
zp6994 <1079552384@qq.com>
2020-05-14 12:47:50 +0800

    文件上传，前端修改

--- customs/testapp/forms.py
+++ customs/testapp/forms.py
@@ -2,5 +2,6 @@ from django import forms
 
 
 class FileUploadForm(forms.Form):
-    name = forms.CharField(max_length=20, min_length=3, required=True, label='名称:')
+    #name = forms.CharField(max_length=20, min_length=3, required=True, label='名称:')
+    #my_file = forms.FileField(label='文件名称:', style="font-size: 30px;")
     my_file = forms.FileField(label='文件名称:')
